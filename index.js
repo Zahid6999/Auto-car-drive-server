9ddb19c4070eb40508ef6c3dd75525cdc317c1eb
habllu <zahidhasan6999@gmail.com>
2022-11-26 20:36:00 +0600

    added jwt access token

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 
@@ -49,6 +50,20 @@ async function run() {
             const result = await bookingsCollection.insertOne(bookings);
             res.send(result);
         });
+
+        // jwt token--------
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token })
+            }
+            console.log(user);
+            res.status(403).send({ accessToken: '' });
+        })
 
         // user create-----
         app.post('/users', async (req, res) => {
