b9a5a1e96e8d870496851be35c38872002b489d7
habllu <zahidhasan6999@gmail.com>
2022-11-26 12:02:05 +0600

    set mongodb, .env, get carCategory data to display and booking data post database

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 
 // middleware--------
 app.use(cors());
 app.use(express.json());
+
+
+// MongoBD---------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixkdtuw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const carCategoryCollection = client.db('AutoCarServer').collection('carCategory');
+        const bookingsCollection = client.db('AutoCarServer').collection('bookings');
+
+
+        //   Get --------
+        app.get('/carCategory', async (req, res) => {
+            const query = {};
+            const options = await carCategoryCollection.find(query).toArray();
+            res.send(options);
+        });
+
+        // Post--------
+        app.post('/bookings', async (req, res) => {
+            const bookings = req.body;
+            console.log(bookings);
+            const result = await bookingsCollection.insertOne(bookings);
+            res.send(result);
+        })
+
+
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
+
 
 
 
