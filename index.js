6ab74765789472c5b9a27efcabe0af903252cf08
habllu <zahidhasan6999@gmail.com>
2022-11-26 17:27:11 +0600

    booking data and user information save

--- index.js
+++ index.js
@@ -19,23 +19,43 @@ async function run() {
     try {
         const carCategoryCollection = client.db('AutoCarServer').collection('carCategory');
         const bookingsCollection = client.db('AutoCarServer').collection('bookings');
+        const usersCollection = client.db('AutoCarServer').collection('users');
 
 
         //   Get --------
+        //   Get the carCategory--------
         app.get('/carCategory', async (req, res) => {
             const query = {};
             const options = await carCategoryCollection.find(query).toArray();
             res.send(options);
         });
 
+        // login Email diye booking query-------
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
+
+
+
         // Post--------
+        // carCategory booking create
         app.post('/bookings', async (req, res) => {
             const bookings = req.body;
             console.log(bookings);
             const result = await bookingsCollection.insertOne(bookings);
             res.send(result);
-        })
+        });
 
+        // user create-----
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
 
     }
